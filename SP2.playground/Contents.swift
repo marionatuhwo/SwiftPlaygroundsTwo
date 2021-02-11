dad8a5b5f5ed51008176488aefb64af102ce36f4
Marion <ano@hawaii.edu>
2021-02-10 15:34:38 -1000

    Solutions posted for Problem set 1, 2, & 3

--- SP2.playground/Contents.swift
+++ SP2.playground/Contents.swift
@@ -55,3 +55,56 @@ else
 {
         print("I'm not so sure about coding in Swift")
 }
+
+/*Problem Set 1:
+Create two variables of type Int (integer)
+Use the > or < operator in your conditional statement and create an if/else statement
+Set up your conditional statement so that it evaluates to false
+In each code block of the if/else statement, print a string of your choice.
+*/
+
+var myFirstNumber = 5
+var mySecondNumber = 8
+
+if myFirstNumber > mySecondNumber
+{
+    print("This Statement will not print")
+    
+}else{
+    print("The Conditional statement evaluates to false and therefore this statement will print")
+}
+
+
+/*Problem Set 2:
+Create two variables of type String
+Use the equal to operator in your conditional statement and create an if statement
+Set up your conditional statement so that it evaluates to true
+In the code block, print the concatenation of your two string variables.
+*/
+
+var myFirstString = "My strings match"
+var mySecondString = "My strings match"
+
+if myFirstString == mySecondString
+{
+    print(myFirstString+mySecondString)
+}
+
+
+/*
+ Problem Set 3:
+ Create two boolean variables
+ Create a conditional statement that evaluates to false
+ Create an if/else statement and in each code block of the if/else statement, print a string of your choice.
+ */
+
+var resultOne = true
+var resultTwo = false
+
+if (resultTwo)
+{
+    print("This statement will not print")
+}
+else {
+    print("The variable resultTwo is false and therefore this statement will print")
+}
